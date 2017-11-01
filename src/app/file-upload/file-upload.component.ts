d4298c950018d6bcad7f5858405d5f54b35480af
Brian Margolis <brian.m.margolis@gmail.com>
2017-11-01 10:59:10 -0500

    Add error messaging for bad uploads

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -12,19 +12,26 @@ import { Http, RequestOptions, Headers } from '@angular/http';
 })
 export class FileUploadComponent {
   @ViewChild('fileInput') fileInput
+  alert = ""
   constructor(private http: Http) { }
 
   upload() {
     const files: FileList = this.fileInput.nativeElement.files;
     if (files.length === 0) {
+      this.alert = "No file provided!"
       return;
     };
+    if (files[0].type !== "text/csv") {
+      this.alert = "Not a CSV file!"
+      return;
+    }
 
     const formData = new FormData();
     formData.append(files[0].name, files[0]);
 
     console.log(files)
 
+    this.alert = ""
     this.http
       .post('http://127.0.0.1:5000/', formData)
       .subscribe();
