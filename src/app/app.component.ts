7dce416b6871fdefff604e9275b3a9f45ccd548f
Brian Margolis <brian.m.margolis@gmail.com>
2017-10-31 20:58:55 -0500

    Replace starter with file upload demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {
+  Ng4FilesStatus,
+  Ng4FilesSelected
+} from 'angular4-files-upload';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  public selectedFiles;
+
+  public filesSelect(selectedFiles: Ng4FilesSelected): void {
+    if (selectedFiles.status !== Ng4FilesStatus.STATUS_SUCCESS) {
+      this.selectedFiles = selectedFiles.status;
+      return;
+
+      // Hnadle error statuses here
+    }
+
+    var file = selectedFiles.files[0]
+
+    var reader = new FileReader();
+    reader.onload = function(progressEvent){
+      // Entire file
+      console.log(this.result);
+    };
+    reader.readAsText(file);
+
+    this.selectedFiles = Array.from(selectedFiles.files).map(file => file.name);
+    console.log(this.selectedFiles)
+  }
 }
